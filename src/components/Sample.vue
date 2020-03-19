6ed139df843542b8aacb16a48a5d77a6a305b68e
lisns <z.z.iz.0pnup@gmail.com>
2020-03-19 13:32:22 +0900

    QAのサンプル構造作成

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -1,44 +1,87 @@
 <template>
   <div class="sample">
-    <h1>Test Question</h1>
-    <div id="question">
-      <ul>
-        <li v-for="item in questions" :key="item.text">
-          {{ item.id }} {{ item.text }}
-        </li>
-      </ul>
+    <h1>Test</h1>
 
+    <div class="qa" v-if="!isAnswer">
+      <div class="question" v-for="item in items" :key="item.id">
+        <p>Q{{ item.id }}</p>
+        <p>{{ item.question }}</p>
+        <div class="answers">
+          <label v-for="(answer, id) in item.answer">
+            <input type="radio" :value="id" v-model="item.select" />
+            {{ answer }}
+          </label>
+        </div>
+        <p>{{ item.select }}</p>
+      </div>
       <button @click="onAnswer">Commit</button>
     </div>
-    <div id="answer" v-show="isAnswer">
-      <p>answer</p>
-    </div>
+
+    <div class="result" v-if="isAnswer">
+      <p>result</p>
+      <p>Your selections</p>
+      <div class="question" v-for="item in items" :key="item.id">
+        <p>Q{{ item.id }}</p>
+        <p>{{ item.question }}</p>
+        <div class="answers">
+          <p>{{ item.select }} : {{ item.answer[item.select] }}</p>
+        </div>
+      </div>
+      <button @click="onBack">Back</button>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Sample',
-  data () {
+  name: "Sample",
+  data() {
     return {
       isAnswer: false,
       questions: [
         { id: "1", text: "item1" },
         { id: "2", text: "item2" },
         { id: "3", text: "item3" },
-        { id: "4", text: "item4" },
-      ]
-    }
+        { id: "4", text: "item4" }
+      ],
+      items: [
+        {
+          id: 1,
+          question: "Q1 text...",
+          answer: ["a1", "b1", "c1"],
+          select: -1
+        },
+        {
+          id: 2,
+          question: "Q2 text...",
+          answer: ["a2", "b2", "c2"],
+          select: -1
+        }
+      ],
+    };
   },
   methods: {
     onAnswer(event) {
-      this.isAnswer = true;
+      var isOK = true;
+      for (let i = 0; i < this.items.length; i++) {
+        const item = this.items[i];
+        if (item.select < 0) {
+          isOK = false;
+          break;
+        }
+      }
+      this.isAnswer = isOK;
+    },
+    onBack(event) {
+      this.isAnswer = false;
+      items.forEach(item => {
+        item.select = -1;
+      });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
